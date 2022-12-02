667adf68e639ae59c7a7b15abb9542a60c9478aa
Steve Wargolet <stephen.wargolet@kohls.com>
2022-12-02 14:44:39 -0600

    2nd process for day2b

--- src/main/java/com/sw/advent/days/Day2b.java
+++ src/main/java/com/sw/advent/days/Day2b.java
@@ -6,9 +6,6 @@ import lombok.Getter;
 
 public class Day2b implements Day {
 
-  @Getter
-  private final String day = "Day2";
-
   private static final Map<String, Integer> LINE_SCORES = Map.of(
       "B X", 1,
       "C X", 2,
@@ -20,9 +17,16 @@ public class Day2b implements Day {
       "A Z", 8,
       "B Z", 9
   );
+  @Getter
+  private final String day = "Day2";
 
   @Override
   public void process(String contents) {
+    process1(contents);
+    process2(contents);
+  }
+
+  public void process1(String contents) {
     String[] lines = contents.split("\n");
 
     int sum = Arrays.stream(lines)
@@ -30,5 +34,27 @@ public class Day2b implements Day {
         .sum();
 
     System.out.println(sum);
+  }
+
+  public void process2(String contents) {
+    String[] lines = contents.split("\n");
+
+    int sum = Arrays.stream(lines)
+        .map(String::toCharArray)
+        .map(chars -> new int[]{chars[0] - 64, chars[2] - 23 - 64 - 2})
+        .mapToInt(nums -> calcThrow(nums[0], nums[1]) + ((nums[1] + 1) * 3))
+        .sum();
+
+    System.out.println(sum);
+  }
+
+  private int calcThrow(int p1, int res) {
+    int temp = p1 + res;
+    if (temp > 3) {
+      return 1;
+    } else if (temp < 1) {
+      return 3;
+    }
+    return temp;
   }
 }
