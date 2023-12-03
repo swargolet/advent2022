c29f1d0deec82328ebee9784203f7aa29fea55d3
Steve Wargolet <stephen.wargolet@kohls.com>
2023-12-02 23:16:00 -0600

    Refactor String.split solution

--- src/main/java/com/sw/advent/days/y2023/Y2023D02.java
+++ src/main/java/com/sw/advent/days/y2023/Y2023D02.java
@@ -15,25 +15,23 @@ public class Y2023D02 implements Day<Integer> {
   //  record Game(int id, int blue)
   @Override
   public Integer part1(String contents) {
+    // Stirng.split
     int sum = contents.lines()
         .mapToInt(line -> {
           String[] split = line.split(":");
           int gameId = Integer.parseInt(split[0].substring(5));
           String gameData = split[1];
 
-          long count = Arrays.stream(gameData.split("[,;]"))
+          boolean isImpossible = Arrays.stream(gameData.split("[,;]"))
               .map(dice -> dice.split(" "))
-              .filter(roll -> ("green".equals(roll[2]) && Integer.parseInt(roll[1]) > MAX_GREEN)
+              .anyMatch(roll -> ("green".equals(roll[2]) && Integer.parseInt(roll[1]) > MAX_GREEN)
                   || ("blue".equals(roll[2]) && Integer.parseInt(roll[1]) > MAX_BLUE)
-                  || ("red".equals(roll[2]) && Integer.parseInt(roll[1]) > MAX_RED))
-              .count();
+                  || ("red".equals(roll[2]) && Integer.parseInt(roll[1]) > MAX_RED));
 
-          return count == 0 ? gameId : 0;
+          return isImpossible ? 0 : gameId;
         }).sum();
 
-    // 2716
-
-//
+    // by character
     int sum2 = contents.lines()
         .mapToInt(line -> {
           String[] split = line.split(":");
@@ -53,17 +51,26 @@ public class Y2023D02 implements Day<Integer> {
           }
           return gameId;
         }).sum();
-//    System.out.println(sum);
-//    System.out.println(sum2);
     System.out.println(sum == sum2);
-    System.out.println(2716 == sum);
     return sum;
   }
 
 
   @Override
   public Integer part2(String contents) {
-    return contents.lines()
+    // Stirng.split
+    int sum = contents.lines()
+        .map(line -> Arrays.stream(line.split(":")[1].split("[,;]"))
+            .map(dice -> dice.split(" "))
+            .reduce(new int[3], (cubeAry, roll) -> { // red = 0, blue = 1, green = 2
+              cubeAry[roll[2].length() - 3] = Math.max(cubeAry[roll[2].length() - 3], Integer.parseInt(roll[1]));
+              return cubeAry;
+            }, (a, b) -> a))
+        .mapToInt(cubeAry -> cubeAry[0] * cubeAry[1] * cubeAry[2])
+        .sum();
+
+    // By character
+    int sum2 = contents.lines()
         .mapToInt(line -> {
           String gameData = line.split(":")[1];
           int maxRed = 0;
@@ -86,5 +93,7 @@ public class Y2023D02 implements Day<Integer> {
           }
           return maxRed * maxGreen * maxBlue;
         }).sum();
+    System.out.println(sum == sum2);
+    return sum;
   }
 }
